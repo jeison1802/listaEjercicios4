b5f5ea0ddf634537b93e3cfcef090a886225475b
jeison1802 <126401829+jeison1802@users.noreply.github.com>
2023-04-02 02:39:17 -0500

    Ejercicios

---
+++ main.py
@@ -0,0 +1,13 @@
+
+import  ejercicio2
+import ejercicio3
+import ejercicio4 
+import ejercicio5
+#import ejercicio6 
+
+if __name__  == "__main__":
+    print(ejercicio2)
+    print(ejercicio3)
+    print(ejercicio4)
+    print(ejercicio5)
+    #print(ejercicio6)
